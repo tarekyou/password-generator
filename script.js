ab0cd9ee21f4e1426d28d8e53edfb3246e1a76c6
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-09-29 17:29:26 -0700

    created arrays to randomize password

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Assignment code here
-
+var passwordLengthC = [];
+var passwordUpperC = ["Q","W","E","R","T","Y","U","I","O","P","L","K","J","H","G","F","D","S","A","Z","X","C","V","B","N","M"];
+var passwordLowerC = ["q","w","e","r","y","u","i","o","p","l","k","j","h","g","f","d","s","a","z","x","c","v","b","n","m"];
+var passwordSpecialC = ["!","#","$","%","&","(",")","*","+",",","-",".","/",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
+var passwordNumberC = ["1","2","3","4","5","6","7","8","9","0"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
