f8b793fbd78ce983f0690989a09606438d137fa1
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-09-29 18:06:39 -0700

    added passwordlengthc function to reset between password generation

--- script.js
+++ script.js
@@ -87,6 +87,8 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+  passwordLengthC = [];
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
