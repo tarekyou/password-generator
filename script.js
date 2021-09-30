fcdd02a97a1525939d2f57d31206cfc28ff26ff3
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-09-29 17:37:07 -0700

    created password length function

--- script.js
+++ script.js
@@ -5,6 +5,23 @@ var passwordLowerC = ["q","w","e","r","y","u","i","o","p","l","k","j","h","g","f
 var passwordSpecialC = ["!","#","$","%","&","(",")","*","+",",","-",".","/",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
 var passwordNumberC = ["1","2","3","4","5","6","7","8","9","0"];
 
+
+
+var passwordLength = function(){
+  var length = window.prompt("What is the desired length of the password?");
+  if (length < 8 ){
+    window.alert("minimum length 8");
+     passwordLength();
+  }
+  else if(length > 128 ){
+    window.alert("maximum length 128");
+    passwordLength();
+  }
+  else  {
+    return length;
+  };
+};
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
